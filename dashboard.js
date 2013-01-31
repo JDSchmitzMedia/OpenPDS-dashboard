cfea0dac29d1cfe4bd437c6f63f0ae1ca1f221af
Ubuntu <ubuntu@domU-12-31-39-10-05-4B.compute-1.internal>
2013-01-31 15:38:36 +0000

    added a list based dashboard for visualizing multiple authorization grants

--- dashboard.js
+++ dashboard.js
@@ -5,19 +5,21 @@ var data = {
     {   firstName: "John",
         lastName: "Smith"}
     ]};
+
 var template = "Employees:<ul>{{#employees}}" +
                             "<li>{{firstName}} {{lastName}}</li>" +
                             "{{/employees}}</ul>";
+
 var html = Mustache.to_html(template, data);
 $('#sampleArea').html(html);
 
 
-$.getJSON('http://celldata.media.mit.edu:8042/account/dashboard', function(data) {
+$.getJSON('http://celldata.media.mit.edu:8050/account/dashboard', function(data) {
     var template = $('#dashboardTpl').html();
-    var dashdata = {
-	client: data.tokens_issued[0].client,
-	key: data.tokens_issued[0].scopes[0].key,
-	description: data.tokens_issued[0].scopes[0].description};
+    var dashdata = {data: data.tokens_issued};
+//	client: data.tokens_issued[0].client,
+//	key: data.tokens_issued[0].scopes[0].key,
+//	description: data.tokens_issued[0].scopes[0].description};
     var html = Mustache.to_html(template, dashdata);
     $('#sampleArea').html(html);
 });
